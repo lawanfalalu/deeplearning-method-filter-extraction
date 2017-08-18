166fe99e880c9ee89439f78fb8654645b9de21cc
lawanfalalu <flawan@aust.edu.ng>
2017-08-18 16:30:02 +0100

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -1,1 +1,6 @@
+#importing tensorflow recommended modules for compatability issues 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
+import tensorflow as tf
